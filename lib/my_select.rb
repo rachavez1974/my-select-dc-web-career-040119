e0b99b55f014f22e65bd4bb3e26062ff639daca0
Ramon Chavez <rachavez1974@gmail.com>
2019-03-24 20:53:18 -0400

    Finish my_select function

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  message = nil
+
+  old_index = 0
+  new_index = 0
+  if collection.empty?
+    return message = "Collection empty"
+  else
+    while old_index < collection.length
+       if yield(collection[old_index]) 
+        new_collection[new_index] = collection[old_index]
+        new_index += 1
+       end
+       old_index += 1
+    end 
+    return new_collection
+  end
+
 end
